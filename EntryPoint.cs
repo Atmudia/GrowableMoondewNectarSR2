bbaaada7b54cbb34c316c57788b2406dde01bcb1
KomiksPL <Komikspl07@gmail.com>
2023-02-27 18:19:59 +0100

    Upgraded to net 6

--- EntryPoint.cs
+++ EntryPoint.cs
@@ -1,9 +1,8 @@
 ﻿using System;
 using System.Linq;
 using HarmonyLib;
-using Il2CppSystem.IO;
+using Il2Cpp;
 using MelonLoader;
-using Sony.NP;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using static GrowableMoondewNectar.EntryPoint;
@@ -52,51 +51,51 @@ namespace GrowableMoondewNectar
             DisabledGameObject.gameObject.SetActive(false);
             Object.DontDestroyOnLoad(DisabledGameObject.gameObject);
             DisabledGameObject.gameObject.hideFlags |= HideFlags.HideAndDontSave;
-            Action<Scene, LoadSceneMode> scene = (scene1, scene2) =>
-            {
-                if (!scene1.name.Equals("GameCore")) return;
-                TreeMoonflower01.prefab = Object.Instantiate(GetOrDefault<ResourceGrowerDefinition>("TreeMoonflower01").prefab, EntryPoint.DisabledGameObject);
-                TreeMoonflower01.primaryResourceType = GetOrDefault<IdentifiableType>("MoondewNectar");
-                TreeMoonflower01.resources = new[]
-                {
-                    new ResourceSpawnerDefinition.WeightedResourceEntry()
-                    {
-                            weight = 1, minimumAmount = 5,
-                            resourceIdentifiableType = TreeMoonflower01.primaryResourceType
-                    }
-                };
-                var treeMoonflower01Prefab = TreeMoonflower01.prefab;
-                var spawnResourceInChildren = treeMoonflower01Prefab.GetComponentInChildren<SpawnResource>();
-                SlimeVarietyModulesStatic.GetCopyOf(treeMoonflower01Prefab.AddComponent<SpawnResource>(), spawnResourceInChildren);
-                Object.Destroy(spawnResourceInChildren);
-                    
-                var treeMesh = treeMoonflower01Prefab.transform.Find("treeMesh").gameObject;
-                var localPositionY = treeMesh.transform.localPosition.y;
-                Object.Instantiate(treeMesh, treeMesh.transform.parent).transform.localPosition = new Vector3(3.75f, localPositionY, -3.75f);
-                Object.Instantiate(treeMesh, treeMesh.transform.parent).transform.localPosition = new Vector3(-3.75f, localPositionY, 3.75f);
-                Object.Instantiate(treeMesh, treeMesh.transform.parent).transform.localPosition = new Vector3(-3.75f, localPositionY, -3.75f);
-                    
-                Object.Instantiate(treeMesh, treeMesh.transform.parent).transform.localPosition = new Vector3(3.75f, localPositionY, 3.75f);
-                var spawnResource = treeMoonflower01Prefab.GetComponent<SpawnResource>();
-                spawnResource.resourceGrowerDefinition = TreeMoonflower01;
-                var spawnJoint = spawnResource.SpawnJoints[0];
-                var spawnJoint1 = Object.Instantiate(spawnJoint.gameObject, spawnJoint.transform.parent);
-                spawnJoint1.transform.localPosition = new Vector3(3.75f, 0, -3.75f);
-                var spawnJoint2 = Object.Instantiate(spawnJoint.gameObject, spawnJoint.transform.parent);
-                spawnJoint2.transform.localPosition = new Vector3(-3.75f, 0, 3.75f);
-                var spawnJoint3 = Object.Instantiate(spawnJoint.gameObject, spawnJoint.transform.parent);
-                spawnJoint3.transform.localPosition = new Vector3(-3.75f, 0, -3.75f);
-                var spawnJoint4 = Object.Instantiate(spawnJoint.gameObject, spawnJoint.transform.parent);
-                spawnJoint4.transform.localPosition = new Vector3(3.75f, 0, 3.75f);
+        }
 
-                Joint[] joints = {
-                    spawnJoint, spawnJoint1.GetComponent<Joint>(), spawnJoint2.GetComponent<Joint>(), spawnJoint3.GetComponent<Joint>(), spawnJoint4.GetComponent<Joint>()
-                };
-                spawnResource.SpawnJoints = joints;
-                spawnResource.resourceGrowerDefinition.maxResources = 5;
-                spawnResource.resourceGrowerDefinition.minResources = 3;
+        public override void OnSceneWasLoaded(int buildIndex, string scene1)
+        {
+            if (!scene1.Equals("GameCore")) return;
+            TreeMoonflower01.prefab = Object.Instantiate(GetOrDefault<ResourceGrowerDefinition>("TreeMoonflower01").prefab, EntryPoint.DisabledGameObject);
+            TreeMoonflower01.primaryResourceType = GetOrDefault<IdentifiableType>("MoondewNectar");
+            TreeMoonflower01.resources = new[]
+            {
+                new ResourceSpawnerDefinition.WeightedResourceEntry()
+                {
+                    weight = 1, minimumAmount = 5,
+                    resourceIdentifiableType = TreeMoonflower01.primaryResourceType
+                }
             };
-            SceneManager.add_sceneLoaded(scene);
+            var treeMoonflower01Prefab = TreeMoonflower01.prefab;
+            var spawnResourceInChildren = treeMoonflower01Prefab.GetComponentInChildren<SpawnResource>();
+            SlimeVarietyModulesStatic.GetCopyOf(treeMoonflower01Prefab.AddComponent<SpawnResource>(), spawnResourceInChildren);
+            Object.Destroy(spawnResourceInChildren);
+                    
+            var treeMesh = treeMoonflower01Prefab.transform.Find("treeMesh").gameObject;
+            var localPositionY = treeMesh.transform.localPosition.y;
+            Object.Instantiate(treeMesh, treeMesh.transform.parent).transform.localPosition = new Vector3(3.75f, localPositionY, -3.75f);
+            Object.Instantiate(treeMesh, treeMesh.transform.parent).transform.localPosition = new Vector3(-3.75f, localPositionY, 3.75f);
+            Object.Instantiate(treeMesh, treeMesh.transform.parent).transform.localPosition = new Vector3(-3.75f, localPositionY, -3.75f);
+                    
+            Object.Instantiate(treeMesh, treeMesh.transform.parent).transform.localPosition = new Vector3(3.75f, localPositionY, 3.75f);
+            var spawnResource = treeMoonflower01Prefab.GetComponent<SpawnResource>();
+            spawnResource.resourceGrowerDefinition = TreeMoonflower01;
+            var spawnJoint = spawnResource.SpawnJoints[0];
+            var spawnJoint1 = Object.Instantiate(spawnJoint.gameObject, spawnJoint.transform.parent);
+            spawnJoint1.transform.localPosition = new Vector3(3.75f, 0, -3.75f);
+            var spawnJoint2 = Object.Instantiate(spawnJoint.gameObject, spawnJoint.transform.parent);
+            spawnJoint2.transform.localPosition = new Vector3(-3.75f, 0, 3.75f);
+            var spawnJoint3 = Object.Instantiate(spawnJoint.gameObject, spawnJoint.transform.parent);
+            spawnJoint3.transform.localPosition = new Vector3(-3.75f, 0, -3.75f);
+            var spawnJoint4 = Object.Instantiate(spawnJoint.gameObject, spawnJoint.transform.parent);
+            spawnJoint4.transform.localPosition = new Vector3(3.75f, 0, 3.75f);
+
+            Joint[] joints = {
+                spawnJoint, spawnJoint1.GetComponent<Joint>(), spawnJoint2.GetComponent<Joint>(), spawnJoint3.GetComponent<Joint>(), spawnJoint4.GetComponent<Joint>()
+            };
+            spawnResource.SpawnJoints = joints;
+            spawnResource.resourceGrowerDefinition.maxResources = 5;
+            spawnResource.resourceGrowerDefinition.minResources = 3;
         }
     }
 }
